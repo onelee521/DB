098f0ee12669c6a424955cfd6874a633e84eb1fe
oneye <onelee521@gmail.com>
2023-12-28 14:05:40 +0900

    sql문 추가

--- 15_build_in_function.sql
+++ 15_build_in_function.sql
@@ -44,6 +44,105 @@ SELECT TRIM('    MySQL    '), TRIM(BOTH '@' FROM '@@@@MySQL@@@@');
 SELECT TRIM('    MySQL    '), TRIM(LEADING '@' FROM '@@@@MySQL@@@@'); -- LRIM
 SELECT TRIM('    MySQL    '), TRIM(TRAILING '@' FROM '@@@@MySQL@@@@'); -- RLIM
 
+-- FORMAT(숫자, 소수점 자리수)
+SELECT FORMAT(1231234566, 3);
+
+-- BIN(숫자), OCT(숫자), HEX(숫자)
+SELECT BIN(65), OCT(65), HEX(65);
+
+-- REPEAT(문자열, 횟수)
+SELECT repeat('hello ', 5);
+
+-- REPLACE(문자열, 찾을 문자열, 바꿀 문자열)
+SELECT REPLACE('마리아DB','마리아','Maria'); -- db값이 바뀌는게 아님
+
+-- REVERSE(문자열)
+SELECT REVERSE('happiness');
+
+-- SPACE(길이)
+SELECT CONCAT('제 포켓몬은', SPACE(3), '이고, 속성은', SPACE(6), '입니다.');
+
+-- SUBSTRING(문자열, 시작위치, 길이)
+SELECT   SUBSTRING('열심히 db공부를 해봅시다.', 5, 4)
+		 , SUBSTRING('열심히 db공부를 해봅시다.', 11); -- 11번째 문자부터 끝까지
+
+-- SUBSTRING_INDEX(문자열, 구분자, 횟수)
+SELECT
+		 SUBSTRING_INDEX('010-123-1234', '-', 2)
+	  , SUBSTRING_INDEX('010-123-1234', '-', -1);
+
 -- 2. 숫자 관련 함수
+-- ABS(숫자)
+SELECT ABS(-123);
+
+-- CEILING(숫자), FLOOR(숫자), ROUND(숫자)
+SELECT CEILING(1234.56), FLOOR(1234.56),ROUND(1234.56);
+
+-- CONV(숫자, 원래 진수, 변환할 진수)
+SELECT CONV('A', 16, 10), CONV('A', 16, 2);
+
+-- MOD(숫자1, 숫자2)
+SELECT MOD(10, 3), 10%3;
+
+-- POW(숫자1, 숫자2), SQRT(숫자)
+SELECT POW(3, 2), SQRT(81);
+
+-- RAND()
+-- RAND()*생성할 난수의 개수 + 난수의 초기값
+SELECT RAND(), FLOOR(RAND() * (11 - 1) + 1);
+
+-- SIGN(숫자)
+SELECT SIGN(10.1), SIGN(0), SIGN(-1.1);
+
+-- TRUNCATE(숫자, 정수)
+SELECT TRUNCATE(12345.12345, 2), TRUNCATE(12345.12345, -2);
 
 -- 3. 날짜 및 시간 관련 함수
+-- ADDDATE(날짜, 차이), SUBDATE(날짜, 차이)
+SELECT ADDDATE('2020-02-01', INTERVAL 28 DAY) -- 윤년까지 고려 가능
+		, ADDDATE('2020-02-01', 28); -- 숫자만 써도 가능
+SELECT SUBDATE('2020-02-01', INTERVAL 1 DAY);
+		, SUBDATE('2020-02-01', 1);
+		
+SELECT ADDDATE('2020-02-01', INTERVAL 28 MONTH); -- month는 영어 붙여줘야 가능
+		, SUBDATE('2020-02-01', INTERVAL 1 MONTH);
+
+-- ADDTIME(날짜/시간, 시간), SUBTIME(날짜/시간, 시간)
+SELECT  ADDTIME('2023-12-28 10:27:00', '1:0:10')
+		, SUBTIME('2023-12-28 10:27:00', '1:0:10');
+		
+-- CURDATE(), CURTIME(), NOW(), SYSDATE()
+SELECT CURDATE(), CURTIME(), NOW(), SYSDATE();
+
+SELECT CURTIME(), CURRENT_TIME(), CURRENT_TIME; -- 괄호 안써도 가능
+
+SELECT @@GLOBAL.TIME_ZONE; -- 현재 타임존 기준을 확인하는 조회문 
+
+-- YEAR(날짜), MONTH(날짜), DAY(날짜)
+SELECT YEAR(CURDATE()), MONTH(CURDATE()), DAY(CURDATE()), DAY(CAST('2023-12-28' AS DATE));
+SELECT HOUR(CURTIME()), MINUTE(CURTIME()), SECOND(CURTIME());
+
+SELECT DATE(NOW()), TIME(NOW());
+
+-- DATEDIFF(날짜1, 날짜2), TIMEDIFF(날짜1 또는 시간1, 날짜2 또는 시간 2)
+SELECT DATEDIFF('2024-06-14', '2023-12-28'), TIMEDIFF('18:00:00', CURTIME());
+
+
+-- DAYOFWEEK(날짜), MONTH(날짜), DAYOFYEAR(날짜)
+SELECT DAYOFWEEK(CURDATE()), MONTHNAME(CURDATE()), DAYOFYEAR(CURDATE());
+
+-- LAST_DAY(날짜)
+SELECT LAST_DAY('20230201');
+
+-- MAKEDATE(연도, 정수)
+SELECT MAKEDATE(2023, 35);
+
+-- MAKETIME(시, 분, 초)
+SELECT MAKETIME(17, 03, 02);
+
+
+-- QUARTER(날짜)
+SELECT QUARTER('2023-12-28');
+
+-- TIME_TO_SEC(시간)
+SELECT TIME_TO_SEC(CURTIME()); -- 오늘 흐른 시간 
